9092639f96afac5bf658d294c5f7557f2a9f6b1d
Dzmitry <zhdv27@mail.ru>
2021-08-11 12:04:59 +0300

    commit 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("qweqweq");
+
 
 ////--------------------------------------------------------
 //        List<String> list = new ArrayList<>();
