c5380fdf9a0052da5fdab3693545c95b9edda634
Dzmitry <zhdv27@mail.ru>
2021-08-11 12:00:28 +0300

    commit  chery

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("chery");
+
 
 ////--------------------------------------------------------
 //        List<String> list = new ArrayList<>();
